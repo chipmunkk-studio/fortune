60c0d8083c58c902558ae360b669fb604ca164e1
john.zar <melow2@naver.com>
2023-09-01 10:55:25 +0900

    [Supabase] 서버 변경.

--- supabase/migrations/20230827032544_remote_commit.sql
+++
@@ -1,5 +0,0 @@
-drop trigger if exists "push_alarm" on "public"."push_alarm";
-
-CREATE TRIGGER push_alarm AFTER INSERT ON public.push_alarm FOR EACH ROW EXECUTE FUNCTION supabase_functions.http_request('https://bpxfujgpliwavynkkpuq.functions.supabase.co/push_alarm', 'POST', '{"Content-type":"application/json"}', '{}', '1000');
-
-
--- supabase/migrations/20230831040713_remote_commit.sql
+++
@@ -1,3 +0,0 @@
-alter table "public"."missions" drop column "is_global";
-
-
