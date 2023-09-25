78d0bf91b27057a1f716b7d09fc3988d6d4aab48
john.zar <melow2@naver.com>
2023-09-25 17:27:31 +0900

    사용하지 않는 폰트 삭제.

--- lib/core/gen/fonts.gen.dart
+++ lib/core/gen/fonts.gen.dart
@@ -10,20 +10,8 @@
 class FontFamily {
   FontFamily._();
 
-  /// Font family: Pretendard
-  static const String pretendard = 'Pretendard';
-
-  /// Font family: Pretendard-Black
-  static const String pretendardBlack = 'Pretendard-Black';
-
   /// Font family: Pretendard-Bold
   static const String pretendardBold = 'Pretendard-Bold';
-
-  /// Font family: Pretendard-ExtraBold
-  static const String pretendardExtraBold = 'Pretendard-ExtraBold';
-
-  /// Font family: Pretendard-ExtraLight
-  static const String pretendardExtraLight = 'Pretendard-ExtraLight';
 
   /// Font family: Pretendard-Light
   static const String pretendardLight = 'Pretendard-Light';
