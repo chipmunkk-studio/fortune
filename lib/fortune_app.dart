b0f06e3e2449b68c2b540a5cc04ed55612ee5b97
john.zar <melow2@naver.com>
2023-11-05 14:51:35 +0900

    [17차] 웹뷰 코드리팩.13

--- lib/fortune_app.dart
+++ lib/fortune_app.dart
@@ -2,7 +2,6 @@ import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:flutter_web_frame/flutter_web_frame.dart';
 import 'package:fortune/core/gen/colors.gen.dart';
 import 'package:fortune/core/navigation/fortune_app_router.dart';
 import 'package:fortune/core/navigation/fortune_web_router.dart';
@@ -16,58 +15,72 @@ class FortuneApp extends StatelessWidget {
 
   const FortuneApp(
     this.startRoute, {
-    super.key,
-  });
+    Key? key,
+  }) : super(key: key);
 
   @override
-  Widget build(BuildContext context) => FlutterWebFrame(
-        maximumSize: const Size(475.0, 812.0), // Maximum size
-        builder: (BuildContext context) {
-          return ScreenUtilInit(
-            designSize: const Size(390, 844),
-            splitScreenMode: false,
-            minTextAdapt: true,
-            rebuildFactor: RebuildFactors.change,
-            builder: (BuildContext context, Widget? child) {
-              return MediaQuery(
-                // 화면 비율 일정 하도록 함.
-                data: MediaQueryData.fromView(WidgetsBinding.instance.window).copyWith(textScaleFactor: 1.0),
-                child: SkeletonTheme(
-                  shimmerGradient: const LinearGradient(
-                    colors: [
-                      ColorName.grey700,
-                      ColorName.grey600,
-                      ColorName.grey600,
-                      ColorName.grey800,
-                      ColorName.grey600,
-                      ColorName.grey600,
-                      ColorName.grey700,
-                    ],
-                    stops: [0.3, 0.5, 0.7, 0.9, 0.7, 0.5, 0.3],
-                  ),
-                  child: MaterialApp(
-                    // 기본적으로 필요한 언어 설정
-                    debugShowCheckedModeBanner: false,
-                    navigatorObservers: [serviceLocator<RouteObserver<PageRoute>>()],
-                    theme: theme(),
-                    onGenerateRoute: _getRouter(),
-                    initialRoute: startRoute,
-                    localizationsDelegates: context.localizationDelegates,
-                    supportedLocales: context.supportedLocales,
-                    locale: context.locale,
-                  ),
-                ),
-              );
-            },
-          );
+  Widget build(BuildContext context) => ScreenUtilInit(
+        designSize: const Size(390, 844),
+        splitScreenMode: false,
+        minTextAdapt: true,
+        rebuildFactor: RebuildFactors.change,
+        builder: (BuildContext context, Widget? child) {
+          if (_isDesktop(context)) {
+            return Center(
+              child: SizedBox(
+                width: 600,
+                height: MediaQuery.of(context).size.height,
+                child: _buildAppContent(context),
+              ),
+            );
+          }
+          return _buildAppContent(context);
         },
       );
 
-  _getRouter() {
+  Widget _buildAppContent(BuildContext context) {
+    return MediaQuery(
+      data: MediaQueryData.fromView(WidgetsBinding.instance.window).copyWith(textScaleFactor: 1.0),
+      child: SkeletonTheme(
+        shimmerGradient: const LinearGradient(
+          colors: [
+            ColorName.grey700,
+            ColorName.grey600,
+            ColorName.grey600,
+            ColorName.grey800,
+            ColorName.grey600,
+            ColorName.grey600,
+            ColorName.grey700,
+          ],
+          stops: [0.3, 0.5, 0.7, 0.9, 0.7, 0.5, 0.3],
+        ),
+        child: MaterialApp(
+          debugShowCheckedModeBanner: false,
+          navigatorObservers: [serviceLocator<RouteObserver<PageRoute>>()],
+          theme: theme(),
+          onGenerateRoute: _getRouter(),
+          initialRoute: startRoute,
+          localizationsDelegates: context.localizationDelegates,
+          supportedLocales: context.supportedLocales,
+          locale: context.locale,
+        ),
+      ),
+    );
+  }
+
+  RouteFactory _getRouter() {
     if (kIsWeb) {
       return serviceLocator<FortuneWebRouter>().router.generator;
     } else {
       return serviceLocator<FortuneAppRouter>().router.generator;
     }
+  }
+
+  bool _isDesktop(BuildContext context) {
+    if (kIsWeb) {
+      final width = MediaQuery.of(context).size.width;
+      return width > 800; // 임의의 값을 설정하여 데스크탑 크기를 판별. 필요에 따라 수정 가능.
+    }
+    return false;
   }
 }
